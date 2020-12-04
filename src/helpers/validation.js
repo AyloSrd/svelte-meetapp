ac21c142b145de2466760d9123c10cf14219550a
AyloSrd <aylo.srd@gmail.com>
2020-12-04 15:30:38 +0100

    added helpers

---
+++ src/helpers/validation.js
@@ -0,0 +1,9 @@
+export const  isEmpty  = val => {
+  return val.trim().length === 0
+}
+
+export const isValidEmail = val => {
+  return new RegExp(
+    "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"
+  ).test(val)
+}
